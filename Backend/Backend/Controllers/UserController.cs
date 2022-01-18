7851321943aaaf99ee92647f054383b0e494c608
Carl Emil Lidbom <lidbom.calle@outlook.com>
2022-01-18 14:04:09 +0100

    User controller and service from old project.

---
+++ Backend/Backend/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using BackEnd.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController()
+        {
+            _userService = new UserService();
+        }
+
+        [HttpGet]
+        public void GetAllUser()
+        {
+            var result = _userService.GetAllUser();
+        }
+    }
+}
---
+++ Backend/Backend/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using BackEnd.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Services
+{
+    public class UserService
+    {
+        private readonly grupp3forumContext _Db;
+
+        public UserService()
+        {
+            _Db = new grupp3forumContext();
+        }
+
+        public IEnumerable<User> GetAllUser()
+        {
+            return _Db.Users.Where(x => x.UserName == "Larzzons");
+        }
+
+        public bool UpdateUser(User user)
+        {
+            try
+            {
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+    }
+}
