dad9b529bbfd618ee6a6aacb6670e8f18d6ab11d
Armin-Ljajic <73130506+Armin-Ljajic@users.noreply.github.com>
2022-01-18 13:58:33 +0100

    Added message controller and service

---
+++ Backend/Backend/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models.Database;
+using Backend.Services;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly MessageService _messageService;
+
+        public MessageController()
+        {
+            _messageService = new MessageService();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Message>> DeleteMessage(int id)
+        {
+            return await _messageService.DeleteMessage(id);
+        }
+    }
+}
---
+++ Backend/Backend/Services/MessageService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Services
+{
+    public class MessageService
+    {
+        private readonly grupp3forumContext _Db;
+
+        public MessageService()
+        {
+            _Db = new grupp3forumContext();
+        }
+
+
+        public async Task<ActionResult<Message>> DeleteMessage(int id)
+        {
+            var message = await _Db.Messages.FindAsync(id);
+            _Db.Messages.Remove(message);
+
+            return message;
+
+        }
+    }
+}
