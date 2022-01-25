47364359838c215b57c331fa70b0ba04be06ae66
Fredrik Tvingstedt <mrt86@live.se>
2022-01-25 15:33:36 +0100

    Update Navbar.vue
    Fixat styling på nav.

--- frontend/src/components/Navbar/Navbar.vue
+++ frontend/src/components/Navbar/Navbar.vue
@@ -2,46 +2,37 @@
   <div class="d-flex justify-content-center p-4 mb-3 bg-dark">
 
             <div>
-                <router-link class="nav-link link-light" to="/">
-                    Home
-                </router-link>
+            <router-link class="nav-link-white-blue"  to="/">
+            Hem |
+            </router-link>|
             </div>
 
-              <div>
-                <router-link class="nav-link link-light" to="/forum">
-                    Forum
-                </router-link>
+            <div>
+            <router-link class="nav-link-white-blue"  to="/forum">
+            Forum |
+            </router-link>|
             </div>
-
-                    <div>
-                <router-link class="nav-link link-light" to="/dashboard">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                     </svg>
-                </router-link>
-                  </div>
     
-            <span v-if="isLoggedIn"
-            >
-            <button class="btn btn-primary" @click="signOut()">Logout</button>
+            <span v-if="isLoggedIn">
+            
+            <router-link class="nav-link-white-blue"  to="/dashboard">
+            Profilsida |
+            </router-link>|
+            
+            <a class="nav-link-white-blue" @click="signOut()">Logga ut</a>|
+            
             </span>
+
             <span v-else>
-              
-              <div>
-            <router-link class="nav-link link-light" to="/register-user"> 
-              Register </router-link> 
-              </div>
-
-              <div>
-            <router-link class="nav-link link-light" to="/signin"> 
-              Login </router-link>
+            <div>
+            <router-link class="nav-link-white-blue" to="/register-user"> 
+            Bli medlem | </router-link>|
+             
+            <router-link class="nav-link-white-blue"  to="/signin"> 
+            Logga in </router-link>|
             </div>
-            </span>
-    
-      
-   </div>
- 
-
+            </span> 
+  </div>
 </template>
 
 <script>
@@ -55,7 +46,7 @@
 </style>
 
 <script setup>
-  import { ref } from 'vue' // used for conditional rendering
+  import { ref} from 'vue' // used for conditional rendering
   import firebase from 'firebase'
   import { useRouter } from 'vue-router'
   const router = useRouter()
@@ -73,3 +64,15 @@
     router.push('/')
   }
 </script>
+
+
+<style>
+  .nav-link-white-blue {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+   
+    font-size: larger;
+    text-decoration: none;
+    color: white;
+    
+  }
+</style>
