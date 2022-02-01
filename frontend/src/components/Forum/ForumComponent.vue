54cf8b7fedd3e1dc77218afd7c23e96a5bfd3012
Sebastian Larsson <seblarsson87@gmail.com>
2022-02-01 13:29:42 +0100

    possible to see all categorys and all threads

--- frontend/src/components/Forum/ForumComponent.vue
+++ frontend/src/components/Forum/ForumComponent.vue
@@ -1,150 +1,137 @@
 <template>
-  <div class="row d-flex justify-content-center">
-    <div class="col-md-8 mt-5">
-      <h5 class="card-header">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="bi bi-unlock-fill"
-          viewBox="0 0 16 16"
-          style="text-align:center;"
-        >
-          <path
-            d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z"
-          />
-        </svg>
-        Forum
-      </h5>
-      <br />
-      <h6
-        style="text-align:center"
-      >Välkommen till forumet {{ user.displayName }}! Om du kan se den här sidan är du redan inloggad!</h6>
-      <br />
-      <br />
-      <br />
-      <h5 class="card-header">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="bi bi-pc-display-horizontal"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M1.5 0A1.5 1.5 0 0 0 0 1.5v7A1.5 1.5 0 0 0 1.5 10H6v1H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5v-1h4.5A1.5 1.5 0 0 0 16 8.5v-7A1.5 1.5 0 0 0 14.5 0h-13Zm0 1h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5ZM12 12.5a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0Zm2 0a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0ZM1.5 12h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1ZM1 14.25a.25.25 0 0 1 .25-.25h5.5a.25.25 0 1 1 0 .5h-5.5a.25.25 0 0 1-.25-.25Z"
-          />
-        </svg>
-        Dator & IT
-      </h5>
-      <br />
-      <br />
-      <br />
-      <h5 class="card-header">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-currency-exchange"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M0 5a5.002 5.002 0 0 0 4.027 4.905 6.46 6.46 0 0 1 .544-2.073C3.695 7.536 3.132 6.864 3 5.91h-.5v-.426h.466V5.05c0-.046 0-.093.004-.135H2.5v-.427h.511C3.236 3.24 4.213 2.5 5.681 2.5c.316 0 .59.031.819.085v.733a3.46 3.46 0 0 0-.815-.082c-.919 0-1.538.466-1.734 1.252h1.917v.427h-1.98c-.003.046-.003.097-.003.147v.422h1.983v.427H3.93c.118.602.468 1.03 1.005 1.229a6.5 6.5 0 0 1 4.97-3.113A5.002 5.002 0 0 0 0 5zm16 5.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0zm-7.75 1.322c.069.835.746 1.485 1.964 1.562V14h.54v-.62c1.259-.086 1.996-.74 1.996-1.69 0-.865-.563-1.31-1.57-1.54l-.426-.1V8.374c.54.06.884.347.966.745h.948c-.07-.804-.779-1.433-1.914-1.502V7h-.54v.629c-1.076.103-1.808.732-1.808 1.622 0 .787.544 1.288 1.45 1.493l.358.085v1.78c-.554-.08-.92-.376-1.003-.787H8.25zm1.96-1.895c-.532-.12-.82-.364-.82-.732 0-.41.311-.719.824-.809v1.54h-.005zm.622 1.044c.645.145.943.38.943.796 0 .474-.37.8-1.02.86v-1.674l.077.018z"
-          />
-        </svg>
-        Ekonomi
-      </h5>
-      <br />
-      <br />
-      <br />
-      <h5 class="card-header">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="bi bi-house-door-fill"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"
-          />
-        </svg>
-        Hem,bostad & familj
-      </h5>
-      <br />
-      <br />
-      <br />
-      <h5 class="card-header">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="bi bi-music-note-list"
-          viewBox="0 0 16 16"
-        >
-          <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
-          <path fill-rule="evenodd" d="M12 3v10h-1V3h1z" />
-          <path d="M11 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 16 2.22V4l-5 1V2.82z" />
-          <path
-            fill-rule="evenodd"
-            d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"
-          />
-        </svg>
-        Kultur & Media
-      </h5>
-      <br />
-      <br />
-      <br />
-      <h5 class="card-header">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="bi bi-cup-straw"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M13.902.334a.5.5 0 0 1-.28.65l-2.254.902-.4 1.927c.376.095.715.215.972.367.228.135.56.396.56.82 0 .046-.004.09-.011.132l-.962 9.068a1.28 1.28 0 0 1-.524.93c-.488.34-1.494.87-3.01.87-1.516 0-2.522-.53-3.01-.87a1.28 1.28 0 0 1-.524-.93L3.51 5.132A.78.78 0 0 1 3.5 5c0-.424.332-.685.56-.82.262-.154.607-.276.99-.372C5.824 3.614 6.867 3.5 8 3.5c.712 0 1.389.045 1.985.127l.464-2.215a.5.5 0 0 1 .303-.356l2.5-1a.5.5 0 0 1 .65.278zM9.768 4.607A13.991 13.991 0 0 0 8 4.5c-1.076 0-2.033.11-2.707.278A3.284 3.284 0 0 0 4.645 5c.146.073.362.15.648.222C5.967 5.39 6.924 5.5 8 5.5c.571 0 1.109-.03 1.588-.085l.18-.808zm.292 1.756C9.445 6.45 8.742 6.5 8 6.5c-1.133 0-2.176-.114-2.95-.308a5.514 5.514 0 0 1-.435-.127l.838 8.03c.013.121.06.186.102.215.357.249 1.168.69 2.438.69 1.27 0 2.081-.441 2.438-.69.042-.029.09-.094.102-.215l.852-8.03a5.517 5.517 0 0 1-.435.127 8.88 8.88 0 0 1-.89.17zM4.467 4.884s.003.002.005.006l-.005-.006zm7.066 0-.005.006c.002-.004.005-.006.005-.006zM11.354 5a3.174 3.174 0 0 0-.604-.21l-.099.445.055-.013c.286-.072.502-.149.648-.222z"
-          />
-        </svg>
-        Mat, dryck & tobak
-      </h5>
+  <div class="content">
+    
+    <!-- <h1>Main</h1> -->
+    <div v-for="item in AllCategoriesAndThreads" :key="item.id" class="content-item">
+      <div class="name">
+         <router-link :id="item.categoryName" :to="`/categorythreads/${item.id}`" class="category-header">{{item.categoryName}}</router-link>
+         <p>Antal trådar: {{item.newThreads.length}}</p>
+      </div>
+      <div v-for="item2 in item.newThreads " :key="item2.id" class="threads">
+        <div class="left">
+          <h4 class="thread-name">{{item2.topic}}</h4>
+          <p>Antal svar i tråd:{{item2.messages.length}}</p>
+        </div>
+        <div v-for="item3 in item2.messages.slice(0,1)" :key="item3.id" class="right">
+          <h4>{{item3.text}}</h4>
+          <p>{{item3.createdAt}}</p>
+        </div>        
+      
+      </div>
     </div>
+   
   </div>
 </template>
 
 <script>
-import { auth, logOut } from "../../assets/js/firebase";
-
-export default {
-  data() {
-    return {
-      user: {},
-    };
-  },
-  created() {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.user = user;
-        console.log(user)
-      } else {
-        this.user = '';
+  export default {
+    components:{
+    },
+    data(){
+      return{
+        categoryNames: [],
       }
-    });
-  },
-  methods: {
-    signOut() {
-      logOut();
-      auth.onAuthStateChanged(() => {
-        this.$router.push("/");
-      });
+    },
+    async created(){
+      await this.loadCategories()
+      await this.$store.dispatch('fetchCategoriesAndThreads')
+    },
+    methods:{
+    async loadCategories(){
+        await this.$store.dispatch('fetchCategories')
+        this.categoryNames=this.$store.state.categories
+      }
+    },
+    computed:{
+      categories(){
+        return this.$store.state.categories
+      },
+
+      AllCategoriesAndThreads(){
+        return this.$store.state.AllCategoriesAndThreads
+      }
     }
   }
-};
 </script>
+
+<style>
+*{
+  padding:0;
+  margin:0;
+  box-sizing:border-box;
+}
+
+h1 {
+  font-size: 100;
+  color: #044e74;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #FB8500;
+}
+.content{
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  width:100%;
+}
+.threads{
+  border:1px solid black;
+  width:75%;
+  display:flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+
+.thread-name{
+  display:flex;
+  justify-content: flex-start;
+  
+}
+
+.name{
+  width:75%;
+  background-color:#333; 
+  color:white;
+  display:flex;
+  flex-direction:column;
+  align-items:start;
+}
+.left{
+  width:50%;
+  border-right:1px solid black;
+  display:flex;
+  flex-direction:column;
+  align-items:start;
+  background-color:#eff0f1;
+}
+.right{
+  width:50%;
+  background-color:#e3e3e3;
+  display:flex;
+  flex-direction:column;
+  align-items:start;
+
+}
+.content-item{
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+}
+
+.category-header{
+  font-size: 150%;
+  text-decoration: none;
+}
+</style>
