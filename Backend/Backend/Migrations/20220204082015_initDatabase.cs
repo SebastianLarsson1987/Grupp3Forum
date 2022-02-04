ca3c914d08ee9c8500f58935688385f1b79fb500
Sebastian Larsson <seblarsson87@gmail.com>
2022-02-04 09:28:45 +0100

    fixed databse errors med by Sebastian

---
+++ Backend/Backend/Migrations/20220204082015_initDatabase.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class initDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeletedUsers",
+                columns: table => new
+                {
+                    UserUid = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DeletionDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportedMessages",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    Email = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    DelatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ThreadId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    UserName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    Email = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    Banned = table.Column<bool>(type: "bit", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    firstName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    lastName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users_1", x => x.Uid);
+                    table.ForeignKey(
+                        name: "FK_Users_Role",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewThread",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Topic = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    Text = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    CreateadAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UserUid = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewThread", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NewThread_Category",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_NewThread_Users",
+                        column: x => x.UserUid,
+                        principalTable: "Users",
+                        principalColumn: "Uid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ThreadId = table.Column<int>(type: "int", nullable: false),
+                    UserUId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    IsReported = table.Column<bool>(type: "bit", nullable: true),
+                    IsDeleated = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Message_NewThread",
+                        column: x => x.ThreadId,
+                        principalTable: "NewThread",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Message_Users",
+                        column: x => x.UserUId,
+                        principalTable: "Users",
+                        principalColumn: "Uid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Message_Reply",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    MessageId = table.Column<int>(type: "int", nullable: false),
+                    UserUid = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message_Reply", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Message_Reply_Message",
+                        column: x => x.MessageId,
+                        principalTable: "Message",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Message_Reply_Users",
+                        column: x => x.UserUid,
+                        principalTable: "Users",
+                        principalColumn: "Uid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_ThreadId",
+                table: "Message",
+                column: "ThreadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_UserUId",
+                table: "Message",
+                column: "UserUId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_Reply_MessageId",
+                table: "Message_Reply",
+                column: "MessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_Reply_UserUid",
+                table: "Message_Reply",
+                column: "UserUid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewThread_CategoryId",
+                table: "NewThread",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewThread_UserUid",
+                table: "NewThread",
+                column: "UserUid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeletedUsers");
+
+            migrationBuilder.DropTable(
+                name: "Message_Reply");
+
+            migrationBuilder.DropTable(
+                name: "ReportedMessages");
+
+            migrationBuilder.DropTable(
+                name: "Message");
+
+            migrationBuilder.DropTable(
+                name: "NewThread");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+        }
+    }
+}
